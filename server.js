08fcf0dcff4e7bb11c812c2ed94509be58ab2bb4
patelsmit31505-rgb <patelsmit31505@gmail.com>
2026-02-26 17:29:15 +0530

    Initialize Express server with static files

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("🔥 App is Live 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running");
+});
